600760087e26c6e89d5a7a1020a239624e92bb50
AJ Brush <19802883+ajb85@users.noreply.github.com>
2022-12-26 22:29:05 -0800

    Only slightly less terrible landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,37 +1,21 @@
 import { Link } from "@remix-run/react";
 import { Logo } from "~/components/atoms";
-import guessesImg from "~/assets/guesses.png";
 import { classNames, combineClasses } from "~/utils";
 
 export default function Index() {
   return (
-    <main className="relative h-[90vh]">
+    <main className="relative h-[90vh] text-white">
       <header
         className={combineClasses(
-          "h-3/4 w-3/4 overflow-hidden drop-shadow-2xl",
+          "flex h-3/4 h-full w-3/4 w-full flex-col items-center overflow-hidden bg-slate-700 p-2 drop-shadow-2xl",
           classNames.absoluteCentered
         )}
       >
-        <img
-          className="absolute z-10 h-full w-1/2 object-cover"
-          src={guessesImg}
-          alt="Wauble's guesses UI"
-        />
-        <img
-          className="absolute right-0 h-full object-cover"
-          src={guessesImg}
-          alt="Wauble's guesses UI"
-        />
-        <div
-          className={combineClasses(
-            "z-20 h-full w-1/2 bg-gradient-to-r from-incorrectGuessBackground via-inWordGuessBackground to-correctGuessBackground",
-            classNames.absoluteCentered
-          )}
-        >
-          <Logo />
-        </div>
-        <Link to="/login">Login</Link>
-        <Link to="/join">Register</Link>
+        <Logo />
+        <nav className="flex w-1/2 justify-between py-80 text-4xl">
+          <Link to="/login">Login</Link>
+          <Link to="/join">Register</Link>
+        </nav>
       </header>
     </main>
   );
