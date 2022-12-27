cced7d77288540cde6c661e72a2eb23265e5aa34
AJ Brush <19802883+ajb85@users.noreply.github.com>
2022-12-26 22:31:09 -0800

    Changes shadow mask on modal to fixed

--- app/components/atoms/Modal/Modal.tsx
+++ app/components/atoms/Modal/Modal.tsx
@@ -10,7 +10,7 @@ export default function Modal(props: Props) {
 
   return (
     <div>
-      <div className="absolute left-0 right-0 top-0 bottom-0 z-10 h-screen w-screen bg-black opacity-20" />
+      <div className="fixed left-0 top-0 z-10 h-screen w-screen bg-black opacity-20" />
       <div className="absolute left-1/2 top-1/2 z-20 -translate-x-2/4 -translate-y-2/4 rounded-md bg-white p-2">
         {props.children}
       </div>
