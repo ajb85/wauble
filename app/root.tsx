76c3f1a1643884a29392483a9b0ad8ea5b26abe6
AJ Brush <19802883+ajb85@users.noreply.github.com>
2023-04-16 14:43:46 -0700

    Adds favicons

--- app/root.tsx
+++ app/root.tsx
@@ -20,6 +20,13 @@ import middleware from "./middleware";
 import { loadUserColorsTerminal } from "./middleware/loadUserColors";
 import type { ColorTheme } from "./models/colorThemes.server";
 
+{
+  /* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+</link> */
+}
 export const links: LinksFunction = () => {
   return [
     { rel: "stylesheet", href: tailwindStylesheetUrl },
@@ -28,6 +35,7 @@ export const links: LinksFunction = () => {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Bungee+Outline&family=Roboto&display=swap",
     },
+    { rel: "manifest", href: "/site.webmanifest" },
   ];
 };
 
